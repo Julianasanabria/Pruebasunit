47cc8bcc0c802aa993d91f09e94e021b6217c4c9
Juliana <Karenjuliana.co@gmail.com>
2026-01-05 10:08:20 -0500

    subiendo pruebas

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Bunit;
+using MiAppBlazor.Components.Pages;
+
+namespace PruebasBlazor;
+
+public class UnitTest1 : TestContext
+{
+    [Fact]
+    public void TestHome()
+    {
+        var component = RenderComponent<Home>();
+        component.Find("h1").MarkupMatches("<h1>Hello, world!</h1>");
+    }
+}
---
+++ ValidadorTests.cs
@@ -0,0 +1,51 @@
+using Xunit;
+using MiAppBlazor.Services;
+
+namespace PruebasBlazor;
+
+public class ValidadorTests
+{
+    [Fact]
+    public void TestEmail()
+    {
+        Validador v = new Validador();
+        bool r = v.ValidarEmail("test@mail.com");
+        Assert.True(r);
+    }
+
+    [Fact]
+    public void TestEmailMalo()
+    {
+        Validador v = new Validador();
+        bool r = v.ValidarEmail("testmailcom");
+        Assert.False(r);
+    }
+
+    [Fact]
+    public void PruebaEdad()
+    {
+        Validador v = new Validador();
+        Assert.True(v.ValidarEdad(20));
+    }
+
+    [Fact]
+    public void PruebaEdadMala()
+    {
+        Validador v = new Validador();
+        Assert.False(v.ValidarEdad(15));
+    }
+
+    [Fact]
+    public void NombrePrueba()
+    {
+        Validador v = new Validador();
+        Assert.True(v.ValidarNombre("Juan"));
+    }
+
+    [Fact]
+    public void NombreMalo()
+    {
+        Validador v = new Validador();
+        Assert.False(v.ValidarNombre("as"));
+    }
+}
